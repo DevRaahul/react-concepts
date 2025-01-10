d6155267f991887b1acec3889e8c08c6d3b2e580
Rahul <DevRaahul>
2025-01-10 19:11:41 +0530

    completed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import "./App.css";
+import CountComponent from "./components/CountComponent";
+import { Button } from "./components/ui/button";
 import { useGlobalContext } from "./context/GlobalContext";
 
 function App() {
   const { globalData, updateGlobalContext } = useGlobalContext();
+
+  const changeTest = () => {
+    updateGlobalContext({ test: "btn clicked" });
+  };
+
   return (
     <>
-      <h2>{globalData.test}</h2>
       <h2>React App</h2>
-      <button onClick={() => updateGlobalContext({ test: "Dummy test data" })} style={{ border: "1px solid black", padding: "5px" }}>
-        {" "}
-        Click me daddy
-      </button>
+      <div className="w-full h-[90vh] flex justify-center items-center gap-4 flex-col">
+        <div>
+          <h3>{"Global Data: " + globalData.test}</h3>
+          <Button onClick={changeTest}>Click me</Button>
+        </div>
+        <CountComponent />
+      </div>
     </>
   );
 }
--- src/components/CountComponent.tsx
+++ src/components/CountComponent.tsx
@@ -1,5 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "./ui/button";
+import { increment } from "@/redux/slices/counterSlice";
+
 const CountComponent = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const data = useSelector((state: any) => state.counter);
+  console.log(data);
+
+  const incrementCount = () => {
+    dispatch(increment());
+  };
+  return (
+    <div>
+      <h3>{"Data from Redux: " + data.count}</h3>
+      <Button onClick={incrementCount}>Count Increment</Button>
+    </div>
+  );
 };
 
 export default CountComponent;
